14a00117f2d14608546b6af3abd66776c98dea10
Samuel <samuel_ho@sla.gov.sg>
2024-09-27 11:01:47 +0800

    NPM

---
+++ 23.0 Backend/23.3 NPM/index.js
@@ -0,0 +1,6 @@
+import gernerateName from "sillyname";
+var sillyName = gernerateName();
+console.log(`My name is ${sillyName}`);
+
+import {randomSuperhero} from "superheroes";
+console.log(`My name is ${randomSuperhero()}!`);
